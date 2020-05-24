6c8ed9cf283da1a9cbdfe659ec3b9f7c4d3146dd
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-05-24 15:55:24 +0300

    Dockerfile update
    Moved october:mirror commant to the "docker-entrypoint.sh" script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,6 +7,9 @@ php artisan route:clear
 # Clear configuration cache
 php artisan config:clear
 
+# Create the public folder
+php artisan october:mirror public --relative
+
 # Run any DB migrations
 php artisan october:up
 
