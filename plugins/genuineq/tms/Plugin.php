b1b4f78beacdb5cdb44f36665f733c9bee184404
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-06-03 09:45:59 +0300

    Added test task

--- plugins/genuineq/tms/Plugin.php
+++ plugins/genuineq/tms/Plugin.php
@@ -79,6 +79,13 @@ class Plugin extends PluginBase
         })->dailyAt('00:00')->when(function () {
             return Carbon::today() == (new Carbon('last day of july'));
         });
+
+
+
+
+        $schedule->call(function () {
+            Mail::rawTo('cosmin.bosutar@genuineq.com', 'Hello friend');
+        })->everyMinute();
     }
 
     public function registerMailTemplates()
