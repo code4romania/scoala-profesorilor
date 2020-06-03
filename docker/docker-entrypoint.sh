b0b2aea78f19762ef9bbe9f13a3c9a877215c2bf
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-06-03 16:40:47 +0300

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,33 +3,6 @@ set -e
 
 # Create .env file from environment variables
 echo APP_ENV=$APP_ENV > .env
-echo APP_DEBUG=$APP_DEBUG\" >> .env
-echo APP_NAME=\"$APP_NAME\" >> .env
-echo APP_URL=\"$APP_URL\" >> .env
-echo APP_KEY=$APP_KEY >> .env
-echo DB_CONNECTION=$DB_CONNECTION >> .env
-echo DB_HOST=$DB_HOST >> .env
-echo DB_PORT=$DB_PORT >> .env
-echo DB_DATABASE=\"$DB_DATABASE\" >> .env
-echo DB_USERNAME=\"$DB_USERNAME\" >> .env
-echo DB_PASSWORD=\"$DB_PASSWORD\" >> .env
-echo CACHE_DRIVER=$CACHE_DRIVER >> .env
-echo SESSION_DRIVER=$SESSION_DRIVER >> .env
-echo SESSION_FILETIME=$SESSION_FILETIME >> .env
-echo QUEUE_DRIVER=$QUEUE_DRIVER >> .env
-echo MAIL_DRIVER=$MAIL_DRIVER >> .env
-echo MAIL_HOST=\"$MAIL_HOST\" >> .env
-echo MAIL_PORT=$MAIL_PORT >> .env
-echo MAIL_USERNAME=\"$MAIL_USERNAME\" >> .env
-echo MAIL_FROM=\"$MAIL_FROM >> .env\" >> .env
-echo MAIL_NAME=\"$MAIL_NAME >> .env\" >> .env
-echo MAIL_PASSWORD=\"$MAIL_PASSWORD\" >> .env
-echo MAIL_ENCRYPTION=$MAIL_ENCRYPTION >> .env
-echo CMS_ACTIVE_THEME=\"$CMS_ACTIVE_THEME\" >> .env
-echo CMS_ROUTES_CACHE=$CMS_ROUTES_CACHE >> .env
-echo CMS_ASSET_CACHE=$CMS_ASSET_CACHE >> .env
-echo CMS_LINK_POLICY=$CMS_LINK_POLICY >> .env
-echo CMS_ENABLE_CSRF=$CMS_ENABLE_CSRF >> .env
 
 # Cache application routes
 php artisan route:clear
