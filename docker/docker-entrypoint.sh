0180446704de2e53670186a574422729dd421c30
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-06-03 18:21:51 +0300

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,11 +8,11 @@ echo APP_LOG=$APP_LOG >> .env
 echo APP_NAME=\"$APP_NAME\" >> .env
 echo APP_URL=\"$APP_URL\" >> .env
 echo APP_KEY=$APP_KEY >> .env
-# echo DB_HOST=\"$DB_HOST\" >> .env
+echo DB_HOST=\"$DB_HOST\" >> .env
 echo DB_PORT=$DB_PORT >> .env
 echo DB_DATABASE=\"$DB_DATABASE\" >> .env
-# echo DB_USERNAME=\"$DB_USERNAME\" >> .env
-# echo DB_PASSWORD=\"$DB_PASSWORD\" >> .env
+echo DB_USERNAME=\"$DB_USERNAME\" >> .env
+echo DB_PASSWORD=\"$DB_PASSWORD\" >> .env
 echo CACHE_DRIVER=$CACHE_DRIVER >> .env
 echo SESSION_DRIVER=$SESSION_DRIVER >> .env
 echo SESSION_FILETIME=$SESSION_FILETIME >> .env
@@ -24,18 +24,18 @@ echo MAIL_USERNAME=\"$MAIL_USERNAME\" >> .env
 echo MAIL_NAME=\"$MAIL_NAME >> .env\" >> .env
 echo MAIL_PASSWORD=\"$MAIL_PASSWORD\" >> .env
 echo MAIL_ENCRYPTION=$MAIL_ENCRYPTION >> .env
-# echo CMS_BACKEND_FORCE_SECURE=$CMS_BACKEND_FORCE_SECURE >> .env
-# echo CMS_ASSET_DEEP_HASHING=$CMS_ASSET_DEEP_HASHING >> .env
-# echo CMS_EDGE_UPDATES=$CMS_EDGE_UPDATES >> .env
-# echo CMS_ASSET_MINIFY=$CMS_ASSET_MINIFY >> .env
-# echo CMS_BACKEND_URI=\"$CMS_BACKEND_URI\" >> .env
-# echo CMS_BACKEND_FORCE_REMEMBER=$CMS_BACKEND_FORCE_REMEMBER >> .env
-# echo CMS_CONVERT_LINE_ENDINGS=$CMS_CONVERT_LINE_ENDINGS >> .env
-# echo CMS_ACTIVE_THEME=\"$CMS_ACTIVE_THEME\" >> .env
-# echo CMS_ROUTES_CACHE=$CMS_ROUTES_CACHE >> .env
-# echo CMS_ASSET_CACHE=$CMS_ASSET_CACHE >> .env
-# echo CMS_LINK_POLICY=$CMS_LINK_POLICY >> .env
-# echo CMS_ENABLE_CSRF=$CMS_ENABLE_CSRF >> .env
+echo CMS_BACKEND_FORCE_SECURE=$CMS_BACKEND_FORCE_SECURE >> .env
+echo CMS_ASSET_DEEP_HASHING=$CMS_ASSET_DEEP_HASHING >> .env
+echo CMS_EDGE_UPDATES=$CMS_EDGE_UPDATES >> .env
+echo CMS_ASSET_MINIFY=$CMS_ASSET_MINIFY >> .env
+echo CMS_BACKEND_URI=\"$CMS_BACKEND_URI\" >> .env
+echo CMS_BACKEND_FORCE_REMEMBER=$CMS_BACKEND_FORCE_REMEMBER >> .env
+echo CMS_CONVERT_LINE_ENDINGS=$CMS_CONVERT_LINE_ENDINGS >> .env
+echo CMS_ACTIVE_THEME=\"$CMS_ACTIVE_THEME\" >> .env
+echo CMS_ROUTES_CACHE=$CMS_ROUTES_CACHE >> .env
+echo CMS_ASSET_CACHE=$CMS_ASSET_CACHE >> .env
+echo CMS_LINK_POLICY=$CMS_LINK_POLICY >> .env
+echo CMS_ENABLE_CSRF=$CMS_ENABLE_CSRF >> .env
 
 # Change .env ownership
 chown www-data:www-data .env
@@ -45,7 +45,7 @@ php artisan route:clear
 
 # Clear & cache configuration cache
 php artisan config:clear
-php artisan config:cache
+# php artisan config:cache
 
 # Create the public folder
 php artisan october:mirror public --relative
@@ -54,7 +54,7 @@ php artisan october:mirror public --relative
 # chown -R www-data:www-data /var/www/public
 
 # Run any DB migrations
-php artisan october:up
+# php artisan october:up
 
 # Reinstall plugins
 php artisan plugin:refresh genuineq.user
