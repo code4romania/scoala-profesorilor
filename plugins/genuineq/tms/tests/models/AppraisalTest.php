6c59eaafeec2b096896208d6b8daafc02042c9a4
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-06-04 16:54:22 +0300

    Update AppraisalTest.php

--- plugins/genuineq/tms/tests/models/AppraisalTest.php
+++ plugins/genuineq/tms/tests/models/AppraisalTest.php
@@ -46,9 +46,9 @@ class AppraisalTest extends PluginTestCase
     public function test__updateAppraisal()
     {
         /** Create a school */
-        $school_1 = $this->helper__createSchool(1);
+        $school_3 = $this->helper__createSchool(3);
         /** Create a teacher */
-        $teacher_2 = $this->helper__createTeacher(2);
+        $teacher_4 = $this->helper__createTeacher(4);
         /** Create a skill 1 */
         $skill_1 = $this->helper__createSkill(1);
         /** Create a skill 2 */
@@ -57,11 +57,11 @@ class AppraisalTest extends PluginTestCase
         $skill_3 = $this->helper__createSkill(3);
 
         /** Create new appraisal */
-        $appraisal = $this->helper__createAppraisal($school_1->id, $teacher_2->id, $skill_1->id, $skill_2->id, $skill_3->id);
+        $appraisal = $this->helper__createAppraisal($school_3->id, $teacher_4->id, $skill_1->id, $skill_2->id, $skill_3->id);
 
         /* Validate created appraisal */
-        $this->assertEquals($school_1->id, $appraisal->school_id);
-        $this->assertEquals($teacher_2->id, $appraisal->teacher_id);
+        $this->assertEquals($school_3->id, $appraisal->school_id);
+        $this->assertEquals($teacher_4->id, $appraisal->teacher_id);
         $this->assertEquals('test-appraisal-objectives', $appraisal->objectives);
         $this->assertEquals($skill_1->id, $appraisal->skill_1_id);
         $this->assertEquals(1, $appraisal->grade_1);
@@ -76,9 +76,9 @@ class AppraisalTest extends PluginTestCase
         $this->assertEquals('new', $appraisal->status);
 
         /** Create a school */
-        $school_3 = $this->helper__createSchool(3);
+        $school_5 = $this->helper__createSchool(5);
         /** Create a teacher */
-        $teacher_4 = $this->helper__createTeacher(4);
+        $teacher_6 = $this->helper__createTeacher(6);
         /** Create a skill 1 */
         $skill_4 = $this->helper__createSkill(4);
         /** Create a skill 2 */
@@ -87,8 +87,8 @@ class AppraisalTest extends PluginTestCase
         $skill_6 = $this->helper__createSkill(6);
 
         /** Update appraisal */
-        $appraisal->school_id = $school_3->id;
-        $appraisal->teacher_id = $teacher_4->id;
+        $appraisal->school_id = $school_5->id;
+        $appraisal->teacher_id = $teacher_6->id;
         $appraisal->objectives = 'new-test-appraisal-objectives';
         $appraisal->skill_1_id = $skill_4->id;
         $appraisal->grade_1 = 4;
@@ -104,8 +104,8 @@ class AppraisalTest extends PluginTestCase
         $appraisal->save();
 
         /* Validate created appraisal */
-        $this->assertEquals($school_3->id, $appraisal->school_id);
-        $this->assertEquals($teacher_4->id, $appraisal->teacher_id);
+        $this->assertEquals($school_5->id, $appraisal->school_id);
+        $this->assertEquals($teacher_6->id, $appraisal->teacher_id);
         $this->assertEquals('new-test-appraisal-objectives', $appraisal->objectives);
         $this->assertEquals($skill_4->id, $appraisal->skill_1_id);
         $this->assertEquals(4, $appraisal->grade_1);
@@ -123,9 +123,9 @@ class AppraisalTest extends PluginTestCase
     public function test__deleteAppraisal()
     {
         /** Create a school */
-        $school_1 = $this->helper__createSchool(1);
+        $school_7 = $this->helper__createSchool(7);
         /** Create a teacher */
-        $teacher_2 = $this->helper__createTeacher(2);
+        $teacher_8 = $this->helper__createTeacher(8);
         /** Create a skill 1 */
         $skill_1 = $this->helper__createSkill(1);
         /** Create a skill 2 */
@@ -134,11 +134,11 @@ class AppraisalTest extends PluginTestCase
         $skill_3 = $this->helper__createSkill(3);
 
         /** Create new appraisal */
-        $appraisal = $this->helper__createAppraisal($school_1->id, $teacher_2->id, $skill_1->id, $skill_2->id, $skill_3->id);
+        $appraisal = $this->helper__createAppraisal($school_7->id, $teacher_8->id, $skill_1->id, $skill_2->id, $skill_3->id);
 
         /* Validate created appraisal */
-        $this->assertEquals($school_1->id, $appraisal->school_id);
-        $this->assertEquals($teacher_2->id, $appraisal->teacher_id);
+        $this->assertEquals($school_7->id, $appraisal->school_id);
+        $this->assertEquals($teacher_8->id, $appraisal->teacher_id);
         $this->assertEquals('test-appraisal-objectives', $appraisal->objectives);
         $this->assertEquals($skill_1->id, $appraisal->skill_1_id);
         $this->assertEquals(1, $appraisal->grade_1);
@@ -168,9 +168,9 @@ class AppraisalTest extends PluginTestCase
     public function test__schoolName()
     {
         /** Create a school */
-        $school_1 = $this->helper__createSchool(1);
+        $school_9 = $this->helper__createSchool(9);
         /** Create a teacher */
-        $teacher_2 = $this->helper__createTeacher(2);
+        $teacher_10 = $this->helper__createTeacher(10);
         /** Create a skill 1 */
         $skill_1 = $this->helper__createSkill(1);
         /** Create a skill 2 */
@@ -179,11 +179,11 @@ class AppraisalTest extends PluginTestCase
         $skill_3 = $this->helper__createSkill(3);
 
         /** Create new appraisal */
-        $appraisal = $this->helper__createAppraisal($school_1->id, $teacher_2->id, $skill_1->id, $skill_2->id, $skill_3->id);
+        $appraisal = $this->helper__createAppraisal($school_9->id, $teacher_10->id, $skill_1->id, $skill_2->id, $skill_3->id);
 
         /* Validate created appraisal */
-        $this->assertEquals($school_1->id, $appraisal->school_id);
-        $this->assertEquals($teacher_2->id, $appraisal->teacher_id);
+        $this->assertEquals($school_9->id, $appraisal->school_id);
+        $this->assertEquals($teacher_10->id, $appraisal->teacher_id);
         $this->assertEquals('test-appraisal-objectives', $appraisal->objectives);
         $this->assertEquals($skill_1->id, $appraisal->skill_1_id);
         $this->assertEquals(1, $appraisal->grade_1);
@@ -205,9 +205,9 @@ class AppraisalTest extends PluginTestCase
     public function test__teacherName()
     {
         /** Create a school */
-        $school_1 = $this->helper__createSchool(1);
+        $school_11 = $this->helper__createSchool(11);
         /** Create a teacher */
-        $teacher_2 = $this->helper__createTeacher(2);
+        $teacher_12 = $this->helper__createTeacher(12);
         /** Create a skill 1 */
         $skill_1 = $this->helper__createSkill(1);
         /** Create a skill 2 */
@@ -216,11 +216,11 @@ class AppraisalTest extends PluginTestCase
         $skill_3 = $this->helper__createSkill(3);
 
         /** Create new appraisal */
-        $appraisal = $this->helper__createAppraisal($school_1->id, $teacher_2->id, $skill_1->id, $skill_2->id, $skill_3->id);
+        $appraisal = $this->helper__createAppraisal($school_11->id, $teacher_12->id, $skill_1->id, $skill_2->id, $skill_3->id);
 
         /* Validate created appraisal */
-        $this->assertEquals($school_1->id, $appraisal->school_id);
-        $this->assertEquals($teacher_2->id, $appraisal->teacher_id);
+        $this->assertEquals($school_11->id, $appraisal->school_id);
+        $this->assertEquals($teacher_12->id, $appraisal->teacher_id);
         $this->assertEquals('test-appraisal-objectives', $appraisal->objectives);
         $this->assertEquals($skill_1->id, $appraisal->skill_1_id);
         $this->assertEquals(1, $appraisal->grade_1);
@@ -242,9 +242,9 @@ class AppraisalTest extends PluginTestCase
     public function test__skillName()
     {
         /** Create a school */
-        $school_1 = $this->helper__createSchool(1);
+        $school_13 = $this->helper__createSchool(13);
         /** Create a teacher */
-        $teacher_2 = $this->helper__createTeacher(2);
+        $teacher_14 = $this->helper__createTeacher(14);
         /** Create a skill 1 */
         $skill_1 = $this->helper__createSkill(1);
         /** Create a skill 2 */
@@ -253,11 +253,11 @@ class AppraisalTest extends PluginTestCase
         $skill_3 = $this->helper__createSkill(3);
 
         /** Create new appraisal */
-        $appraisal = $this->helper__createAppraisal($school_1->id, $teacher_2->id, $skill_1->id, $skill_2->id, $skill_3->id);
+        $appraisal = $this->helper__createAppraisal($school_13->id, $teacher_14->id, $skill_1->id, $skill_2->id, $skill_3->id);
 
         /* Validate created appraisal */
-        $this->assertEquals($school_1->id, $appraisal->school_id);
-        $this->assertEquals($teacher_2->id, $appraisal->teacher_id);
+        $this->assertEquals($school_13->id, $appraisal->school_id);
+        $this->assertEquals($teacher_14->id, $appraisal->teacher_id);
         $this->assertEquals('test-appraisal-objectives', $appraisal->objectives);
         $this->assertEquals($skill_1->id, $appraisal->skill_1_id);
         $this->assertEquals(1, $appraisal->grade_1);
@@ -279,9 +279,9 @@ class AppraisalTest extends PluginTestCase
     public function test__skillDescription()
     {
         /** Create a school */
-        $school_1 = $this->helper__createSchool(1);
+        $school_15 = $this->helper__createSchool(15);
         /** Create a teacher */
-        $teacher_2 = $this->helper__createTeacher(2);
+        $teacher_16 = $this->helper__createTeacher(16);
         /** Create a skill 1 */
         $skill_1 = $this->helper__createSkill(1);
         /** Create a skill 2 */
@@ -290,11 +290,11 @@ class AppraisalTest extends PluginTestCase
         $skill_3 = $this->helper__createSkill(3);
 
         /** Create new appraisal */
-        $appraisal = $this->helper__createAppraisal($school_1->id, $teacher_2->id, $skill_1->id, $skill_2->id, $skill_3->id);
+        $appraisal = $this->helper__createAppraisal($school_15->id, $teacher_16->id, $skill_1->id, $skill_2->id, $skill_3->id);
 
         /* Validate created appraisal */
-        $this->assertEquals($school_1->id, $appraisal->school_id);
-        $this->assertEquals($teacher_2->id, $appraisal->teacher_id);
+        $this->assertEquals($school_15->id, $appraisal->school_id);
+        $this->assertEquals($teacher_16->id, $appraisal->teacher_id);
         $this->assertEquals('test-appraisal-objectives', $appraisal->objectives);
         $this->assertEquals($skill_1->id, $appraisal->skill_1_id);
         $this->assertEquals(1, $appraisal->grade_1);
@@ -320,23 +320,23 @@ class AppraisalTest extends PluginTestCase
     /**
      * Created an appraisal object and return's it
      */
-    protected function helper__createAppraisal($schoolId, $teacherId, $firstSkillId, $secondSkillId, $thirdSkillId)
+    protected function helper__createAppraisal($index, $schoolId, $teacherId, $firstSkillId, $secondSkillId, $thirdSkillId)
     {
         /** Create new learningPlan */
         $appraisal = Appraisal::create([
             'school_id' => $schoolId,
             'teacher_id' => $teacherId,
-            'objectives' => 'test-appraisal-objectives',
+            'objectives' => 'test-appraisal-objectives-' . $index,
             'skill_1_id' => $firstSkillId,
             'grade_1' => 1,
             'skill_2_id' => $secondSkillId,
             'grade_2' => 2,
             'skill_3_id' => $thirdSkillId,
             'grade_3' => 3,
-            'notes_objectives_set' => 'test-appraisal-notes-objectives-set',
-            'notes_objectives_approved' => 'test-appraisal-notes-objectives-approved',
-            'notes_skills_set' => 'test-appraisal-notes-skills-set',
-            'notes_evaluation_opened' => 'test-appraisal-notes-evaluation-opened',
+            'notes_objectives_set' => 'test-appraisal-notes-objectives-set-' . $index,
+            'notes_objectives_approved' => 'test-appraisal-notes-objectives-approved-' . $index,
+            'notes_skills_set' => 'test-appraisal-notes-skills-set-' . $index,
+            'notes_evaluation_opened' => 'test-appraisal-notes-evaluation-opened-' . $index,
             'status' => 'new',
         ]);
 
