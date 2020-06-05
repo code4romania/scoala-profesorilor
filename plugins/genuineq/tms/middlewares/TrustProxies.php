0b1b9f2444c6cde66607ec93cc0e5872767d0c04
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-06-05 22:44:29 +0300

    Update TrustProxies.php

--- plugins/genuineq/tms/middlewares/TrustProxies.php
+++ plugins/genuineq/tms/middlewares/TrustProxies.php
@@ -19,9 +19,10 @@ class TrustProxies extends Middleware
      */
     protected $headers = [
         Request::HEADER_X_FORWARDED_AWS_ELB,
-        Request::HEADER_X_FORWARDED_PROTO,
-        // Request::HEADER_FORWARDED => 'FORWARDED',
-        // Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-        // Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR    => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST   => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT   => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO  => 'X_FORWARDED_PROTO',
     ];
 }
