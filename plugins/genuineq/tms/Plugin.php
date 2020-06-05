2bdf60eed9f913e7eb99687adffe17e8da879b83
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-06-05 18:11:11 +0300

    Added trusted proxies support

--- plugins/genuineq/tms/Plugin.php
+++ plugins/genuineq/tms/Plugin.php
@@ -222,5 +222,13 @@ class Plugin extends PluginBase
                 $profile->save();
             }
         });
+
+        \App::register('\Fideloper\Proxy\TrustedProxyServiceProvider');
+
+        // Add a new middleware to beginning of the stack.
+        $this->app['Illuminate\Contracts\Http\Kernel']->prependMiddleware('Genuineq\Tms\Middlewares\TrustProxies');
+
+        // Add a new middleware to end of the stack.
+        // $this->app['Illuminate\Contracts\Http\Kernel']->pushMiddleware('Path\To\Custom\Middleware');
     }
 }
