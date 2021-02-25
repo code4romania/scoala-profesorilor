c316d165d6ed5ffc84ba7893af8bbbd892a831d5
StefanGhiban <stefanghiban962@gmail.com>
2021-02-25 15:46:53 +0200

    removed the now unused cleaning plugin

--- plugins/zen/cleaner/classes/Panel.php
+++
@@ -1,1 +0,0 @@
-<?php echo \View::make('zen.cleaner::panel');
--- plugins/zen/cleaner/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-Route::match(['get', 'post'], '/zen/cleaner/{action}', function ($action) {
-    if(!\BackendAuth::check())
-        return 'Access error';
-    return App::call('Zen\Cleaner\Actions\\'.$action);
-})->middleware('web');
