4407493d8abb3e06b60c66f942c9e56b6ee8086c
Cosmin Bosutar <cosmin.bosutar@genuineq.com>
2021-03-29 10:19:16 +0300

    Copied partials to assets folder in order to remove inline scripts.

---
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/carousel-multiple-courses.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    $('#carousel-multiple-courses-{{ id }}').on('slide.bs.carousel', function (e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 1;
+        var totalItems = $('.carousel-item-{{ id }}').length;
+
+        if (idx >= totalItems-(itemsPerSlide-1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i=0; i<it; i++) {
+                // append slides to end
+                if (e.direction=="left") {
+                    $('.carousel-item-{{ id }}').eq(i).appendTo('.carousel-inner-{{ id }}');
+                } else {
+                    $('.carousel-item-{{ id }}').eq(0).appendTo('.carousel-inner-{{ id }}');
+                }
+            }
+        }
+    });
+});
