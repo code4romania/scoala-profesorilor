e2b7988129305f021ac47dc11d28859be524c8cf
StefanGhiban <stefanghiban962@gmail.com>
2021-03-29 14:46:51 +0300

    added document ready to the login-modal script

--- themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/auth/login-modal.js
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/auth/login-modal.js
@@ -35,13 +35,16 @@ function submitRecaptchaForm() {
     });
 }
 
-/** Modal functions */
-$("#login-modal").on('shown.bs.modal', function(){
-    /* Hide the body scrollbar. */
-    document.body.style.overflow = "hidden";
-});
 
-$("#login-modal").on('hidden.bs.modal', function(){
-    /* Show the body scrollbar. */
-    document.body.style.overflow = "auto";
-});
+$(document).ready( () => {
+    /** Modal functions */
+    $("#login-modal").on('shown.bs.modal', function(){
+        /* Hide the body scrollbar. */
+        document.body.style.overflow = "hidden";
+    });
+
+    $("#login-modal").on('hidden.bs.modal', function(){
+        /* Show the body scrollbar. */
+        document.body.style.overflow = "auto";
+    });
+})
