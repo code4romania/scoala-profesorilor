f4d7059ffd94428193d329e26dd242d45f9528e9
StefanGhiban <stefanghiban962@gmail.com>
2021-03-29 15:14:56 +0300

    every signup modal inline scripts

---
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/auth/signup-select-modal.js
@@ -0,0 +1,11 @@
+$(document).ready( () => {
+    $("#signup-select-modal").on('show.bs.modal', function(){
+        /* Hide the body scrollbar. */
+        document.body.style.overflow = "hidden";
+    });
+
+    $("#signup-select-modal").on('hidden.bs.modal', function(){
+        /* Show the body scrollbar. */
+        document.body.style.overflow = "auto";
+    });
+})
