162b3c8a635f7ccaa8bc3d96e729590a7eec310a
StefanGhiban <stefanghiban962@gmail.com>
2021-03-31 13:02:22 +0300

    teacher learning plan course pagination inline scripts

---
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/teacher-learning-plans/learning-plan-course-pagination.js
@@ -0,0 +1,23 @@
+$(document).ready( () => {
+    $('.learning-plan-course-search-pagination').on('click', function () {
+        /* Extract the clicked page. */
+        var $newPage = $(this).data('page');
+        /* Extract the clicked learning plan ID. */
+        var $learningPlanId = $(this).data('id');
+
+        /* Select the course select form and send the request */
+        $('#learningPlanCourseSearchForm').request(
+            'onLearningPlanCourseSearch',
+            {
+                update: {
+                    'teacher-learning-plans/learning-plan-course-grid': '#learningPlanCourseSearchResults',
+                    'teacher-learning-plans/learning-plan-course-pagination': '#learningPlanCourseSearchPagination'
+                },
+                data: {
+                    page: $newPage,
+                    learningPlanId: $learningPlanId
+                }
+            }
+        );
+    });
+})
