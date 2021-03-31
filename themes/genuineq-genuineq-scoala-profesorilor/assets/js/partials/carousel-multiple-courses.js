e5135c20dd8c5fa9fdbdb8647ef6010ce3786356
StefanGhiban <stefanghiban962@gmail.com>
2021-03-31 14:49:18 +0300

    carousel multiple courses inline scripts

--- themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/carousel-multiple-courses.js
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/carousel-multiple-courses.js
@@ -1,18 +1,21 @@
 $( document ).ready(function() {
-    $('#carousel-multiple-courses-{{ id }}').on('slide.bs.carousel', function (e) {
+    let jsScript = $("#carousel-multiple-courses");
+    let id = jsScript.attr("data-id");
+
+    $('#carousel-multiple-courses-' + id).on('slide.bs.carousel', function (e) {
         var $e = $(e.relatedTarget);
         var idx = $e.index();
         var itemsPerSlide = 1;
-        var totalItems = $('.carousel-item-{{ id }}').length;
+        var totalItems = $('.carousel-item-' + id).length;
 
         if (idx >= totalItems-(itemsPerSlide-1)) {
             var it = itemsPerSlide - (totalItems - idx);
             for (var i=0; i<it; i++) {
                 // append slides to end
                 if (e.direction=="left") {
-                    $('.carousel-item-{{ id }}').eq(i).appendTo('.carousel-inner-{{ id }}');
+                    $('.carousel-item-' + id).eq(i).appendTo('.carousel-inner-' + id);
                 } else {
-                    $('.carousel-item-{{ id }}').eq(0).appendTo('.carousel-inner-{{ id }}');
+                    $('.carousel-item-' + id).eq(0).appendTo('.carousel-inner-' + id);
                 }
             }
         }
