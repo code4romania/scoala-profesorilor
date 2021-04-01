e27675400f114797d1562ad74f67b6756c4fb5c7
Cosmin Bosutar <cosmin.bosutar@genuineq.com>
2021-04-01 20:32:03 +0300

    Updated logic to send "inspectorates" and "addresses" to the script

--- themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/school-profile/account-tab.js
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/school-profile/account-tab.js
@@ -1,24 +1,17 @@
 $( document ).ready(function() {
     let jsScript = $("#school-account-tab");
+
     let inspectorates = JSON.parse(jsScript.attr("data-inspectorates"));
     let addresses = JSON.parse(jsScript.attr("data-addresses"));
 
-    console.log('inspectorates: ' + inspectorates);
-    console.log('inspectorates keys: ' + Object.keys(inspectorates));
-    let inspectoratesKeys = Object.keys(inspectorates);
-
-    console.log('addresses: ' + addresses);
-    console.log('addresses keys: ' + Object.keys(addresses));
-    let addressesKeys = Object.keys(addresses);
-
     $('#school_inspectorate_id').autocomplete({
-        source: inspectoratesKeys,
+        source: inspectorates,
         highlightClass: 'text-danger',
         treshold: 1
     });
 
     $('#school_address_id').autocomplete({
-        source: addressesKeys,
+        source: addresses ,
         highlightClass: 'text-danger',
         treshold: 1
     });
