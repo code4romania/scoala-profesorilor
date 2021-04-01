30ebeb87ed6bcc12b863d1aec6cb2fc6fac530d3
Cosmin Bosutar <cosmin.bosutar@genuineq.com>
2021-04-01 22:31:54 +0300

    Removed inline script for graph distributed costs

--- themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/school-dashboard/graph-distributed-costs.js
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/school-dashboard/graph-distributed-costs.js
@@ -1,45 +1,52 @@
 $( document ).ready(function() {
     let jsScript = $('#graph-distributed-costs');
+
     let distributedLabels = JSON.parse(jsScript.attr('data-distributedLabels'));
     let distributedCostsLabels = JSON.parse(jsScript.attr('data-distributedCostsLabels'));
     let distributedCosts = JSON.parse(jsScript.attr('data-distributedCosts'));
     let distributedCostsColor = JSON.parse(jsScript.attr('data-distributedCostsColor'));
     let distributedCostsBorderColor = JSON.parse(jsScript.attr('data-distributedCostsBorderColor'));
 
-    console.log(distributedCostsLabels);
-    console.log(distributedCosts);
-
     var ctxB = document.getElementById("barChart").getContext('2d');
+
+    /** Construnct the graph datasets. */
+    let datasets = [];
+    for (const [key, value] of Object.entries(distributedCostsLabels)) {
+        datasets.push({
+            label: value,
+            data: [
+                distributedCosts[key]['0'],
+                distributedCosts[key]['1'],
+                distributedCosts[key]['2'],
+                distributedCosts[key]['3'],
+                distributedCosts[key]['4'],
+                distributedCosts[key]['5']
+            ],
+            backgroundColor: [
+                distributedCostsColor[key],
+                distributedCostsColor[key],
+                distributedCostsColor[key],
+                distributedCostsColor[key],
+                distributedCostsColor[key],
+                distributedCostsColor[key]
+            ],
+            borderColor: [
+                distributedCostsBorderColor[key],
+                distributedCostsBorderColor[key],
+                distributedCostsBorderColor[key],
+                distributedCostsBorderColor[key],
+                distributedCostsBorderColor[key],
+                distributedCostsBorderColor[key]
+            ],
+            borderWidth: 1
+        });
+    }
 
     var myBarChart = new Chart(ctxB, {
         type: 'bar',
         data: {
-            labels: [distributedLabels['0'], distributedLabels['1'], distributedLabels['2'], distributedLabels['3'], distributedLabels['4'], distributedLabels['5']],
-            datasets: [
-                distributedCostsLabels.forEach(distributedCostsLabel => {
-                    return {
-                        label: distributedCostsLabel,
-                        data: [distributedCosts[distributedCostsLabel]['0'], distributedCosts[distributedCostsLabel]['1'], distributedCosts[distributedCostsLabel]['2'], distributedCosts[distributedCostsLabel]['3'], distributedCosts[distributedCostsLabel]['4'], distributedCosts[distributedCostsLabel]['5'] ],
-                        backgroundColor: [
-                            distributedCostsColor[distributedCostsLabel],
-                            distributedCostsColor[distributedCostsLabel],
-                            distributedCostsColor[distributedCostsLabel],
-                            distributedCostsColor[distributedCostsLabel],
-                            distributedCostsColor[distributedCostsLabel],
-                            distributedCostsColor[distributedCostsLabel]
-                        ],
-                        borderColor: [
-                            distributedCostsBorderColor[distributedCostsLabel],
-                            distributedCostsBorderColor[distributedCostsLabel],
-                            distributedCostsBorderColor[distributedCostsLabel],
-                            distributedCostsBorderColor[distributedCostsLabel],
-                            distributedCostsBorderColor[distributedCostsLabel],
-                            distributedCostsBorderColor[distributedCostsLabel]
-                        ],
-                        borderWidth: 1
-                    }
-                })
-            ]
+            labels: [ distributedLabels['0'], distributedLabels['1'], distributedLabels['2'], distributedLabels['3'], distributedLabels['4'], distributedLabels['5'] ],
+            datasets: datasets
         },
         options: {
             scales: {
