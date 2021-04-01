9f8b45d9dbf07fb6db2cb29496213a503fe3717c
Cosmin Bosutar <cosmin.bosutar@genuineq.com>
2021-04-01 23:27:30 +0300

    Removed inline script from graph-skill-matrix

--- themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/school-dashboard/graph-skill-matrix.js
+++ themes/genuineq-genuineq-scoala-profesorilor/assets/js/partials/school-dashboard/graph-skill-matrix.js
@@ -1,32 +1,45 @@
-// $( document ).ready(function() {
-//     var ctxL = document.getElementById("skillMatrix").getContext('2d');
+$( document ).ready(function() {
+    let jsScript = $('#graph-skill-matrix');
 
-//     var myLineChart = new Chart(ctxL, {
-//         type: 'line',
-//         data: {
-//             labels: [
-//                 {% for skillSemestersLabel in skillSemestersLabels %}
-//                     "{{ skillSemestersLabel }}",
-//                 {% endfor %}
-//             ],
-//             datasets: [
-//                 {% for skillSemester in skillSemesters %}
-//                     {
-//                         label: "{{ skillSemester['label'] }}",
-//                         data: [
-//                             {% for skillValue in skillSemester['values'] %}
-//                                 {{ skillValue }},
-//                             {% endfor %}
-//                         ],
-//                         backgroundColor: [ "{{ skillSemester['backgroundColor'] }}", ],
-//                         borderColor: [ "{{ skillSemester['borderColor'] }}", ],
-//                         borderWidth: 2
-//                     },
-//                 {% endfor %}
-//             ]
-//         },
-//         options: {
-//             responsive: true
-//         }
-//     });
-// });
+    let skillSemestersLabels = JSON.parse(jsScript.attr('data-skillSemestersLabels'));
+    let skillSemestersNames = JSON.parse(jsScript.attr('data-skillSemestersNames'));
+    let skillSemestersValues = JSON.parse(jsScript.attr('data-skillSemestersValues'));
+    let skillSemestersBackgroundColors = JSON.parse(jsScript.attr('data-skillSemestersBackgroundColors'));
+    let skillSemestersBorderColor = JSON.parse(jsScript.attr('data-skillSemestersBorderColor'));
+
+    var ctxL = document.getElementById("skillMatrix").getContext('2d');
+
+    /** Construnct the graph labels. */
+    let labels = [];
+    for (const [key, value] of Object.entries(skillSemestersLabels)) {
+        labels.push(skillSemestersLabels[key]);
+    }
+
+    /** Construnct the graph datasets. */
+    let datasets = [];
+    for (const [key, value] of Object.entries(skillSemestersNames)) {
+        let data = [];
+        for (const [dataKey, dataValue] of Object.entries(skillSemestersValues[key])) {
+            data.push(dataValue);
+        }
+
+        datasets.push({
+            label: value,
+            data: data,
+            backgroundColor: [ skillSemestersBackgroundColors[key] ],
+            borderColor: [ skillSemestersBorderColor[key] ],
+            borderWidth: 2
+        });
+    }
+
+    var myLineChart = new Chart(ctxL, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: datasets
+        },
+        options: {
+            responsive: true
+        }
+    });
+});
