b11b6eb2526f41c559448f161d6ad8b9b12afc4f
LucianDex <remindfwd@gmail.com>
2021-09-17 17:40:17 +0300

    [fix] added missing diacritics

--- plugins/genuineq/tms/updates/seniority_levels_seeder.php
+++ plugins/genuineq/tms/updates/seniority_levels_seeder.php
@@ -9,7 +9,7 @@ class SeniorityLevelsSeeder extends Seeder
     {
         SeniorityLevel::create(["name" => "Bacalaureat",   "diacritic" => "Bacalaureat",   "description" => ""]);
         SeniorityLevel::create(["name" => "Licenta",       "diacritic" => "Licență",       "description" => ""]);
-        SeniorityLevel::create(["name" => "Master",        "diacritic" => "Master",        "description" => ""]);
+        SeniorityLevel::create(["name" => "Masterat",      "diacritic" => "Masterat",        "description" => ""]);
         SeniorityLevel::create(["name" => "Doctorat",      "diacritic" => "Doctorat",      "description" => ""]);
         SeniorityLevel::create(["name" => "Post doctorat", "diacritic" => "Post doctorat", "description" => ""]);
     }
